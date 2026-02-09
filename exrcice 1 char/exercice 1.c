0b42b662e71fec07a57a4b07b70cde570fe49d93
S0uf12n8 <oulkadisoufiane69@gmail.com>
2026-02-09 09:19:22 +0100

    ...

--- exrcice 1 char/exercice 1.c
+++ exrcice 1 char/exercice 1.c
@@ -2,75 +2,106 @@
 #include <stdlib.h>
 #include <string.h>
 
-char** lirePhrases(int n){
-	char tmp;
-	char **tab;
-	int i;
-	/*==== allocation du tab ====*/
-	tab=(char**)malloc(n*sizeof(char*));
-	if(tab==NULL) return NULL;
-	
-	/*===== allocation du phrases ====*/
-	for (i=0;i<n;i++){
-		printf("donner la phrase %d ",i);
-		scanf("%s",tmp);
-		tab[i]=(char*)malloc((strlen(tmp)+1)*sizeof(char));
-		strcpy(tab[i],tmp);
-		
-	} 
-	return tab;
+/* ========= lirePhrases ========= */
+char** lirePhrases(int n) {
+    char buffer[101];
+    char **tab = malloc(n * sizeof(char*));
+    int i;
+
+    if (tab == NULL) return NULL;
+
+    for (i = 0; i < n; i++) {
+        printf("Donner la phrase %d : ", i + 1);
+
+        /* read full line (phrase) */
+        scanf(" %[^\n]", buffer);
+
+        tab[i] = malloc((strlen(buffer) + 1) * sizeof(char));
+        if (tab[i] == NULL) return NULL;
+
+        strcpy(tab[i], buffer);
+    }
+    return tab;
 }
 
-void trierParLongueur(char** tab,int n){
-	int j,i;
-	char *tmp;
-	/*==== sort by string lengthg====*/
-	for(i=0;i<n-1;i++){
-		for(j=i+1;j<n;j++){
-			if(strlen(tab[i])>strlen(tab[j])){
-				tmp=tab[i];
-				tab[i]=tab[j];
-				tab[j]=tmp;
-			}
-		}
-	}
-	
-}
-/*========= function of fusionner ================*/
-char* fusionner(char **tab,int n,char separatuer){
-	int i
+/* ========= trierParLongueur ========= */
+void trierParLongueur(char **tab, int n) {
+    int i, j;
+    char *tmp;
 
-	for(i=0;i<n;i++){
-			strcat(tab[i], seprateur);
-		
-	}
-	
-	return phrase;
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (strlen(tab[i]) > strlen(tab[j])) {
+                tmp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = tmp;
+            }
+        }
+    }
 }
 
-void liberer(char **tab,int n){
-	int i;
-	for(i=0;i<n;i++){
-		free(tab[i]);
-	}
-	free(tab);
-	
-	
+/* ========= fusionner ========= */
+char* fusionner(char **tab, int n, char separateur) {
+    int i;
+    int total = 0;
+    char *phrase;
+    char sep[2];
+
+    /* calculate total size */
+    for (i = 0; i < n; i++) {
+        total += strlen(tab[i]);
+    }
+    total += (n - 1); /* separators */
+    total += 1;       /* '\0' */
+
+    phrase = malloc(total * sizeof(char));
+    if (phrase == NULL) return NULL;
+
+    phrase[0] = '\0';
+
+    sep[0] = separateur;
+    sep[1] = '\0';
+
+    for (i = 0; i < n; i++) {
+        strcat(phrase, tab[i]);
+        if (i < n - 1) {
+            strcat(phrase, sep);
+        }
+    }
+    return phrase;
 }
 
-int main(){
-	char seprateur[10];
-	char **tab;
-	char *phrase;
-	
-
-	printf("donner le separtauer : ");
-	scanf("%s",separateur);
-	tab =lirePhrases(n);
-	trierParLongueur(tab,n);
-	phrase = fusionner(tab,n,separateur);
-	liberer(tab,n);
-	free(phrase);
-	
-	
+/* ========= liberer ========= */
+void liberer(char **tab, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        free(tab[i]);
+    }
+    free(tab);
 }
+
+/* ========= main ========= */
+int main() {
+    int n;
+    char separateur;
+    char **tab;
+    char *phrase;
+
+    printf("Donner le nombre de phrases : ");
+    scanf("%d", &n);
+
+    printf("Donner le separateur : ");
+    scanf(" %c", &separateur);
+
+    tab = lirePhrases(n);
+    trierParLongueur(tab, n);
+
+    phrase = fusionner(tab, n, separateur);
+    printf("\nPhrase fusionnee : %s\n", phrase);
+
+    liberer(tab, n);
+    free(phrase);
+
+    return 0;
+}
+
